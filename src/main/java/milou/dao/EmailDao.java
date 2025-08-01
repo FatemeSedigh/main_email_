24acda055c1f5ec59f9467c3ab938b9a57b81194
fati <fatemesadatsedigh@gmail.com>
2025-08-01 14:51:16 +0330

    retry

--- src/main/java/milou/dao/EmailDao.java
+++ src/main/java/milou/dao/EmailDao.java
@@ -6,10 +6,4 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface EmailDao {
-    Email createEmail(Email email, List<User> recipients) throws SQLException;
-    List<Email> getReceivedEmails(int userId, boolean onlyUnread) throws SQLException;
-    List<Email> getSentEmails(int userId) throws SQLException;
-    Email getEmailByCode(String code) throws SQLException;
-    void markAsRead(int emailId, int userId) throws SQLException;
-    boolean canUserReadEmail(int emailId, int userId) throws SQLException;
-}
+   }
--- src/main/java/milou/dao/UserDao.java
+++ src/main/java/milou/dao/UserDao.java
@@ -1,11 +1,4 @@
 package milou.dao;
 
-import milou.model.User;
-
-import java.sql.SQLException;
-
 public interface UserDao {
-    User createUser(User user) throws SQLException;
-    User getUserByEmail(String email) throws SQLException;
-    boolean emailExists(String email) throws SQLException;
 }
